91a96eec42c9dc762182c1b80576a0f5f3067e30
github username <alexngundji@gmail.com>
2021-02-14 16:44:47 -0500

    completed

---
+++ median_of_two.js
@@ -0,0 +1,26 @@
+
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    
+};
+
+const median = (a1, a2) => {
+    let x = a1.concat(a2);
+    x.sort(function (a,b) {
+      return a - b;
+    });
+    let len = x.length;
+    
+    return len%2 === 0 ? (x[Math.floor(len/2)-1] + x[Math.ceil(len/2)])/2 : x[Math.floor(len/2)];
+  
+  }
+  
+  let a = [0,2,3,5,9];
+  let b = [1,4];
+  console.log(median(a,b));
